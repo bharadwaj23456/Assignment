9311a631de76a39cde6b77c4ec793b774be6a00f
bharadwaj23456 <79959667+bharadwaj23456@users.noreply.github.com>
2024-09-02 11:40:46 +0530

    Uploading the code

---
+++ newapplication/newapplication/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  newapplication
+//
+//  Created by Reddy Bharadwaj Chowdary on 31/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ newapplication/newapplication/newapplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  newapplicationApp.swift
+//  newapplication
+//
+//  Created by Reddy Bharadwaj Chowdary on 31/08/24.
+//
+
+import SwiftUI
+
+@main
+struct newapplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
